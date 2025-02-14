ebdf27f434221ef01e9949cc763c329f0addadfd
Arjun GS <arjungsanal@gmail.com>
2025-02-14 11:42:44 +0530

    Production bug fix

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
-import Footer from "./footer";
+
 
 export default function AuthComponent() {
   const [email, setEmail] = useState("");
