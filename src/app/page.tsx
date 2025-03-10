2be2da78f366a22b37a1fa9b55feb1a26fb052b3
Arjun GS <arjungsanal@gmail.com>
2025-03-10 11:17:33 +0530

    Toaster added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 
 import { AuthProvider } from "@/context/AuthContext";
-import { Toaster } from "@/components/ui/toaster";
 import { HomePage } from "@/components/pages/home/home";
 
 export default function Home() {
   return (
     <AuthProvider>    
       <HomePage />
-      <Toaster />
       </AuthProvider>
     
   );
