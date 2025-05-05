016b16643a41002b35f082ea09105be35bf88462
Arjun GS <arjungsanal@gmail.com>
2025-05-05 13:01:14 +0530

    feat(helper) : Implement helper functions

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -5,3 +5,8 @@ export const getPublicUrl = (path: string ) => {
     const { data:image_url } = supabase.storage.from("campaigns").getPublicUrl(path);
     return image_url.publicUrl;
 }
+
+
+const approveCampaign = async (campaignId: string) => {
+    
+} 
